afe7d6d428bca05e1a6193b884fbcfa87c28e23b
Ahmad Shalabi <eng.ahmadshalabi@gmail.com>
2025-01-24 22:47:17 +0000

    Add Command Line User Interface

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -68,12 +68,4 @@ public class DataSource {
     return account;
   }
 
-  public static void main(String[] args) {
-    Customer customer = getCustomer("twest8o@friendfeed.com");
-    System.out.println(customer.getName());
-
-    Account account = getAccount(customer.getAccountId());
-    System.out.println(account.getBalance());
-  }
-
 }
